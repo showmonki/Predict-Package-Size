7afb8c132f9531a5eb7595e9ae87698d13d0d263
Nan Zou <nanzou_leeds@hotmail.com>
2018-05-11 00:15:05 +0800

    finish model part

--- Text mining.py
+++ Text mining.py
@@ -7,19 +7,24 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 
-data =  pd.read_excel("/transfer/LQ rawdata.xlsx")
-data.head()
+raw =  pd.read_excel("/transfer/LQ rawdata.xlsx")
+raw['group'] = ['Predict' if pack =='UNKNOWN' else 'Train' for pack in raw['PACKSIZE']]
+#data.head()
+data = raw[raw['group']=='Train'].copy()
 user_dict = "/transfer/LQ key.txt"
 stop_words_path = "/transfer/stop_words"
 with open(stop_words_path,'r') as f:
 	stop_words = [line.strip() for line in f.readlines()]
 jieba.load_userdict(user_dict)
 
+##keywords weighting
 x = data['PROD_DESC_RAW']+data['ATTRIBUTE']
-#list = jieba.cut(x)
-#tags = jieba.analyse.extract_tags(str(x),topK=20)
-
 x_corpus = []
 for i in x:
 	i = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*【】|:≤\-（）<]+|[\d+]{5,}"," ",i)
@@ -51,9 +56,9 @@ kyws.loc[kyws['word'].str.contains("红酒|葡萄酒|啤酒|威士忌|力娇酒|
 #Generate the dictionary file for all titles. Ranked by tfidf.
 
 
-
+## Model
 data ['words']=""
-for i in range(len(data)):
+for i in data.index.tolist():
 		line = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*【】|:≤\-（）<]+|[\d+]{5,}", " ",data['PROD_DESC_RAW'][i]+data['ATTRIBUTE'][i]).strip()
 		#print(line)
 		seg = jieba.cut(line)
@@ -64,12 +69,28 @@ for i in range(len(data)):
 		data.loc[i,'words']=" ".join(words)
 x = data['words']
 y = data['PACKSIZE']
+
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=1)
-x_train_corpus = []
-vectorizer = TfidfVectorizer()
-tfidf = vectorizer.fit_transform(x_train_corpus)
-x_train_matrix = pd.DataFrame(tfidf.toarray())
-x_train_matrix.columns =vectorizer.get_feature_names()
+
+train_vectorizer = TfidfVectorizer(max_features=500)
+train_tfidf = train_vectorizer.fit_transform(x_train)
+x_train_matrix = pd.DataFrame(train_tfidf.toarray())
+x_train_matrix.columns =train_vectorizer.get_feature_names()
+
+test_vectorizer = TfidfVectorizer(max_features=500)
+test_tfidf = test_vectorizer.fit_transform(x_test)
+x_test_matrix = pd.DataFrame(test_tfidf.toarray())
+x_test_matrix.columns =test_vectorizer.get_feature_names()
+
 clf = KNeighborsClassifier()
-#clf = SVC()
+clf = SVC()
+clf = DecisionTreeClassifier()
+clf = RandomForestClassifier()
+clf = GaussianNB()
+clf = GradientBoostingClassifier()
 clf.fit(x_train_matrix,y_train)
+test_acc = clf.score(x_test_matrix, y_test)
+print(test_acc)
+#y_t = clf.predict(x_test)
+predict = pd.DataFrame({'predict':y_t,'test':y_test})
+
